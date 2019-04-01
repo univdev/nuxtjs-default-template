daabb0ebc0f6e4d5be9477fec8a25d6af9322791
Park Chanyoung <rhdwkrxla68@gmail.com>
2019-04-01 21:18:55 +0900

    플러그인 연동 작업 진행중

---
+++ plugins/element-ui.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import { lang } from 'element-ui/lib/locale/lang/ko';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI, { lang });
